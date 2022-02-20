4204bb38ad95e6bbad3ac48b34535d192bd65e43
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-02-19 21:42:23 -0500

    small fix

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,7 +1,6 @@
 import { useHistory } from "react-router-dom";
 
 export default function Footer() {
-  const history = useHistory();
   return (
     <footer>
       <h2 className="footer-item">give your 🧠 a rest</h2>
